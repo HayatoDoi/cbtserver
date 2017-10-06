7241c9ca000924f7d98183cf4d47a4d6264b5a57
Hayato Doi <b1517914@planet.kanazawa-it.ac.jp>
2017-10-07 08:44:18 +0900

    Dockerで環境を構築出来るようにした。

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # cbtserver
 Web-Based answer submit system(CTF,etc...)
+
+## Difference
+- I artificially increased bugs
+- I made it work with docker
+
+## Linence
+?
+
+## Author
+[HayatoDoi](https://github.com/HayatoDoi/)
--- admin_login.php
+++
@@ -1,8 +0,0 @@
-<?php
-	echo "<html><head><title>Welcome to administration page!</title></head>";
-	echo "<body><form action = \"control.php\" method=\"post\">";
-	echo "<p>username:<input type=\"text\" name=\"username\"></p>";
-	echo "<p>password:<input type=\"password\" name=\"password\"></p>";
-	echo "<input type=\"submit\" value=\"login\">";
-	echo "</body></html>";
-?>
